556094bfcc63090691708858b4799a771d187110
DamonYoung42 <damonyoung@wi.rr.com>
2017-01-12 15:28:07 -0600

    Add project files.

---
+++ src/PalindromeNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PalindromeNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int count = 0;
+            int y = 0;
+ 
+            do
+            {
+                if (y.ToString().Reverse().SequenceEqual(y.ToString()))
+                {
+                    Console.WriteLine(y);
+                    Console.ReadLine();
+                    count++;
+                }
+                y++;
+            }
+            while (count < 2000);
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/PalindromeNumbers/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PalindromeNumbers")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a3009511-45ee-4e17-ba55-f1f7381f8ef1")]
