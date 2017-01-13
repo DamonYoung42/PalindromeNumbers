1ad855fdd143b6f2568676060422eb904bea883b
DamonYoung42 <damonyoung@wi.rr.com>
2017-01-13 13:09:27 -0600

    verifyStringPalindromes()

--- src/PalindromeNumbers/Program.cs
+++ src/PalindromeNumbers/Program.cs
@@ -9,21 +9,65 @@ namespace PalindromeNumbers
     {
         public static void Main(string[] args)
         {
+            countNumberPalindromes();
+            verifyStringPalindrome();
+            Console.ReadLine();
+        }
+
+        public static void countNumberPalindromes()
+        {
             int count = 0;
             int y = 0;
- 
+            int maxCount;
+
+            Console.WriteLine("How many palindrome numbers would you like to see?");
+
+            while (!int.TryParse(Console.ReadLine(), out maxCount) || maxCount <= 0)
+            {
+                Console.WriteLine("Please enter a positive integer");
+            }
+
             do
             {
-                if (y.ToString().Reverse().SequenceEqual(y.ToString()))
+                if (isPalindrome(y))
                 {
                     Console.WriteLine(y);
                     count++;
                 }
                 y++;
             }
-            while (count < 2000);
-            Console.WriteLine(count);
-            Console.ReadLine();
+            while (count < maxCount);
+
+        }
+        public static bool isPalindrome(int z)
+        {
+            return z.ToString().Reverse().SequenceEqual(z.ToString());
+        }
+
+        public static bool isPalindrome(string z)
+        {
+            return z.Reverse().SequenceEqual(z);
+        }
+
+        public static void verifyStringPalindrome()
+        {
+            string test;
+            do
+            {
+                Console.WriteLine("Enter any sequence of characters:");
+                test = Console.ReadLine();
+                if (isPalindrome(test))
+                {
+                    Console.WriteLine("{0} is a palindrome", test);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not a palidrome", test);
+                }
+            }
+            while (test.ToLower() != "exit");
+            Console.WriteLine("Thanks for playing. Goodbye!");
         }
     }
+
 }
